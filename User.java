15bb3e8222f1bd9ec35e5d5d444d058d2ec6dba6
Good Day <goodday@MacBook-Pro-Good.local>
2022-07-17 10:56:32 +0300

    Class User

---
+++ User.java
@@ -0,0 +1,34 @@
+package Planer;
+
+public class User {
+    private static Integer id;
+    private String name;
+    private String lastName;
+    private int ids;
+    static {
+        id = 1;
+    }
+
+    private User(String name, String lastName, int ids) {
+        this.name = name;
+        this.lastName = lastName;
+        this.ids = ids;
+        id++;
+
+    }
+
+    public User(String name, String lastName){
+        this(name, lastName, id);
+    }
+
+    public Integer getId() {
+        return this.ids;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+}
